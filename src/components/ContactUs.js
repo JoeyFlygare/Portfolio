aea223b1deee5068a3290901b43188eff517b93b
sweetbabyjay <jayfly64@gmail.com>
2021-02-27 00:07:15 -0800

    added a link to resume

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -15,6 +15,7 @@ export default class ContactUs extends Component {
           <aside className="eigth columns footer-widgets">
             <div className="widget">
               <h4>Linkedin :<a href={resumeData.linkedinId}> Joey Flygare</a></h4>
+              <h4><a href="https://docs.google.com/document/d/1iSJ3IFNhSmj7lQpyaoN8U1lIdUTCwIjsM_fkbonBGz4/edit?usp=sharing">Resume</a></h4>
             </div>
           </aside>
         </div>
